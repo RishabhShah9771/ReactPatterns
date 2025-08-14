fbef1b096fff71463606a5bf413e2a9196f0e3c9
RISHABH SHAH <rnshah1997@gmail.com>
2025-08-13 20:23:04 -0400

    final Compound Component Update

--- src/components/Accordian/AccordionContent.jsx
+++ src/components/Accordian/AccordionContent.jsx
@@ -1,7 +1,10 @@
 import { useAccordionContext } from "./Accordion";
+import { useAccordionItemContext } from "./AccordionItem";
 
-export default function AccordionContent({ id, children, className }) {
+export default function AccordionContent({ children, className }) {
   const { openItemId } = useAccordionContext();
+  const id = useAccordionItemContext();
+
   const isOpen = openItemId === id;
   return (
     <div
--- src/components/Accordian/AccordionItem.jsx
+++ src/components/Accordian/AccordionItem.jsx
@@ -1,3 +1,22 @@
-export default function AccordionItem({ children, className }) {
-  return <li className={className}>{children}</li>;
+import { createContext, useContext } from "react";
+
+const AccordionItemContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+  if (!ctx) {
+    throw new Error(
+      "AccordionItem - related components must be wrapped by <Accordion.Item>"
+    );
+  }
+  return ctx;
+}
+
+export default function AccordionItem({ id, children, className }) {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
 }
